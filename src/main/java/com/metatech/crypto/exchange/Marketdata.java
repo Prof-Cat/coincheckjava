e338340e6b061fa05d67457076209a3ce9edd5aa
Prof-Cat <mimaopro@outlook.jp>
2023-04-26 23:18:05 +0900

    move methods out, switch to util

--- src/main/java/com/metatech/crypto/exchange/Marketdata.java
+++ src/main/java/com/metatech/crypto/exchange/Marketdata.java
@@ -39,136 +39,27 @@ public class Marketdata {
 
     public String getPublicTicker() {
         String url = targetExchange.getBaseUrl() + "/ticker"; 
-        String jsonString = requestByPublicUrl(url);
+        String jsonString = Util.requestByPublicUrl(url);
         return jsonString;
     }
 
     public String getPublicTrades() {
         String url = targetExchange.getBaseUrl() + "/trades?pair=" + targetExchange.getCoinPair(); 
-        String jsonString = requestByPublicUrl(url);
+        String jsonString = Util.requestByPublicUrl(url);
         return jsonString;
     }
 
     public String getPublicOrderBooks() {
         String url = targetExchange.getBaseUrl() + "/order_books?pair=" + targetExchange.getCoinPair(); 
-        String jsonString = requestByPublicUrl(url);
+        String jsonString = Util.requestByPublicUrl(url);
         return jsonString;
     }
 
     public String getPrivateTicker() {
         String url = targetExchange.getBaseUrl() + "/accounts/ticker?pair=" + targetExchange.getCoinPair();
-        String jsonString = requestByUrlWithHeader(url);
+        String jsonString = Util.requestByUrlWithHeader(url, apiKey, apiSecret);
         return jsonString;
     }
 
-    // GET /api/accounts/balance
-    public String getAccountBalance() {
-        String url = targetExchange.getBaseUrl() + "accounts/balance";
-        String jsonString = requestByUrlWithHeader(url);
-        return jsonString;
-    }
 
-    // GET /api/accounts
-    public String getAccountInfo() {
-        String url = targetExchange.getBaseUrl() + "accounts";
-        String jsonString = requestByUrlWithHeader(url);
-        return jsonString;
-    }
-
-    // GET /api/exchange/orders/opens
-    public String getAccountOrders() {
-        String url = targetExchange.getBaseUrl() + "exchange/orders/opens";
-        String jsonString = requestByUrlWithHeader(url);
-        return jsonString;
-    }
-
-    // GET /api/exchange/orders/transactions_pagination
-    public String getTransactionPages() {
-        String url = targetExchange.getBaseUrl() + "exchange/orders/transactions_pagination";
-        String jsonString = requestByUrlWithHeader(url);
-        return jsonString;
-    }
-
-    // Cancel <id> : DELETE /api/exchange/orders/<id>
-    // Request <id> cancel status : GET /api/exchange/orders/cancel_status?id=[id]
-
-    // GET /api/exchange/orders/transactions
-    public String getTransactions() {
-        String url = targetExchange.getBaseUrl() + "exchange/orders/transactions";
-        String jsonString = requestByUrlWithHeader(url);
-        return jsonString;
-    }
-
-    private String requestByUrlWithHeader(String url){
-        HttpClient client = HttpClient.newHttpClient();
-        String nonce = createNonce();
-        logger.info("sending request to " + url);
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(url))
-                .header("ACCESS-KEY", apiKey)
-                .header("ACCESS-NONCE", nonce)
-                .header("ACCESS-SIGNATURE", createSignature(nonce + url))
-                .GET()
-                .build();
-    
-        // {'ACCESS-KEY': 'yourapi-key', 'ACCESS-NONCE': '1682422247275682048', 'ACCESS-SIGNATURE': 'the signed message'}
-        // send : https://coincheck.com/api/accounts/balance
-
-        String jsonString;
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            jsonString = response.body();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            jsonString = null;
-        }
-        return jsonString;
-    }
-
-    private String createSignature(String nonce) {
-        String message = nonce;
-        return HMAC_SHA256Encode(apiSecret, message);
-    }
-
-    private String createNonce() {
-        long currentUnixTime = System.currentTimeMillis();
-        String nonce = String.valueOf(currentUnixTime);
-        return nonce;
-    }
-
-    private String requestByPublicUrl(String url) {
-        String jsonString;
-        logger.info(url);
-        try {
-            HttpClient httpClient = HttpClient.newHttpClient();
-            HttpRequest httpRequest = HttpRequest.newBuilder()
-                    .uri(URI.create(url))
-                    .build();
-            HttpResponse<String> httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
-            jsonString = httpResponse.body();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-            jsonString = null;
-        }
-        return jsonString;
-    }
-
-    public static String HMAC_SHA256Encode(String secretKey, String message) {
-
-        SecretKeySpec keySpec = new SecretKeySpec(secretKey.getBytes(),"hmacSHA256");
-
-        Mac mac = null;
-        try {
-            mac = Mac.getInstance("hmacSHA256");
-            mac.init(keySpec);
-        } catch (NoSuchAlgorithmException e) {
-            // can't recover
-            throw new RuntimeException(e);
-        } catch (InvalidKeyException e) {
-            // can't recover
-            throw new RuntimeException(e);
-        }
-        byte[] rawHmac = mac.doFinal(message.getBytes());
-        return Hex.encodeHexString(rawHmac);
-    }
 }
