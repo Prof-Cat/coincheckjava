eecb838bea211afa8d96b2860af354956df57d2f
Prof-Cat <mimaopro@outlook.jp>
2023-05-09 20:13:32 +0900

    split coin pair

--- src/main/java/com/metatech/crypto/exchange/ExchangeX.java
+++ src/main/java/com/metatech/crypto/exchange/ExchangeX.java
@@ -8,15 +8,17 @@ public class ExchangeX {
     private final String apiSecret;
     private final String baseUrl;
     public final String exchangeName;
-    public final String coinPair;
+    public final String coin;
+    public final String currency;
     private static final Logger logger = Testslf4j.getLogger(ExchangeX.class);
 
-    public ExchangeX(String xApiKey, String xApiSecret, String xBaseUrl, String xName, String xCoinPair){
+    public ExchangeX(String xApiKey, String xApiSecret, String xBaseUrl, String xName, String xCoin, String xCurrency){
         this.apiKey = xApiKey;
         this.apiSecret = xApiSecret;
         this.baseUrl = xBaseUrl;
         this.exchangeName = xName;
-        this.coinPair = xCoinPair;
+        this.coin = xCoin;
+        this.currency = xCurrency;
         logger.info("Exchange " + xName + " initialized with base url:" + xBaseUrl);
     }
     
@@ -24,5 +26,7 @@ public class ExchangeX {
     public String getSecret(){return this.apiSecret;    }
     public String getBaseUrl(){  return this.baseUrl; }
     public String getExchangeName(){ return this.exchangeName; }
-    public String getCoinPair(){ return this.coinPair; }
+    public String getCoinPair(){ return this.coin + "_" + this.currency; }
+    public String getCoin(){ return this.coin;}
+    public String getCurrency(){ return this.currency;}
 }
