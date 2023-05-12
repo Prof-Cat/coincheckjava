092e6c583af0fbb1066d0fedd8208a6b426ed012
Prof-Cat <mimaopro@outlook.jp>
2023-05-12 22:25:34 +0900

    TreeMap shift

--- src/main/java/com/metatech/crypto/exchange/TradingDatabase.java
+++ src/main/java/com/metatech/crypto/exchange/TradingDatabase.java
@@ -1,44 +1,32 @@
 package com.metatech.crypto.exchange;
 
-public class TradingDatabase {
-    // Get the database information
-    // Element database = (Element) root.getElementsByTagName("database").item(0);
-    // String host = database.getElementsByTagName("host").item(0).getTextContent();
-    // String port = database.getElementsByTagName("port").item(0).getTextContent();
-    // String username = database.getElementsByTagName("username").item(0).getTextContent();
-    // String password = database.getElementsByTagName("password").item(0).getTextContent();
-    private String myDatabase;
-    private String myDBHost;
-    private String myDBPort;
-    private String myDBUserName;
-    private String myDBPassword;
+import java.util.TreeMap;
 
-    public void TradingDatabase(){
-        this.myDatabase = "";
-        this.myDBHost ="";
-        this.myDBPassword = "";
-        this.myDBUserName = "";
-        this.myDBPort = "";
+import org.slf4j.Logger;
+
+import com.metatech.JavaCat.Testslf4j;
+
+public class TradingDatabase {
+    private TreeMap<String, String> tradeDBAttributesTreeMap;
+    private static final Logger logger = Testslf4j.getLogger(TradingDatabase.class);
+
+    public TradingDatabase(){
+        this.tradeDBAttributesTreeMap = new TreeMap<>();
     }
 
     public void init(String xDB, String xHost, String xPort, String xUser, String xPwd){
-        this.myDatabase = xDB;
-        this.myDBHost = xHost;
-        this.myDBPort = xPort;
-        this.myDBUserName = xUser;
-        this.myDBPassword = xPwd;
+    }
+    
+    public void addDBAttribute(String xKey, String xAttributeString){
+        if (tradeDBAttributesTreeMap.containsKey(xKey) ) {
+            logger.info(xKey + " is already exists");
+        } else {
+            tradeDBAttributesTreeMap.put(xKey, xAttributeString);
+            logger.info("adding endpoint of " + xKey + "::" + xAttributeString);
+        }
     }
 
-    public String getDatabase() { return this.myDatabase;}
-    public void setDatabase( String xDB) { this.myDatabase = xDB; }
-    public String getDBHost() { return this.myDBHost; }
-    public void setDBHost( String xDBHost ) { this.myDBHost = xDBHost; }
-    public String getDBPort() { return this.myDBPort; }
-    public void setDBPort( String xDBPort ) { this.myDBPort = xDBPort; }
-    
-    public String getDBUserName () { return this.myDBUserName; }
-    public void setDBUser( String xDBUser ) { this.myDBUserName = xDBUser; }
-    public String getDBPassword() { return this.myDBPassword; }
-    public void setDBPassword( String xDBPassword ) { this.myDBPassword = xDBPassword; }
-
+    public String getDataBaseAttribute(String xAttribute){
+        return this.tradeDBAttributesTreeMap.get(xAttribute);
+    }
 }
