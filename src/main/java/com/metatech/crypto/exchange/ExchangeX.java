c15875f94de6d7cd4b655ddf97a693d0fe2f1a1c
Prof-Cat <mimaopro@outlook.jp>
2023-05-12 22:20:16 +0900

    shift to TreeMap, instead of private data members

--- src/main/java/com/metatech/crypto/exchange/ExchangeX.java
+++ src/main/java/com/metatech/crypto/exchange/ExchangeX.java
@@ -1,32 +1,27 @@
 package com.metatech.crypto.exchange;
 
 import com.metatech.JavaCat.Testslf4j;
+import com.metatech.crypto.exchange.TagMap.ExchangeEnum;
+
+import java.util.TreeMap;
+
 import org.slf4j.Logger;
 
 public class ExchangeX {
-    private final String apiKey;
-    private final String apiSecret;
-    private final String baseUrl;
-    public final String exchangeName;
-    public final String coin;
-    public final String currency;
-    private static final Logger logger = Testslf4j.getLogger(ExchangeX.class);
 
-    public ExchangeX(String xApiKey, String xApiSecret, String xBaseUrl, String xName, String xCoin, String xCurrency){
-        this.apiKey = xApiKey;
-        this.apiSecret = xApiSecret;
-        this.baseUrl = xBaseUrl;
-        this.exchangeName = xName;
-        this.coin = xCoin;
-        this.currency = xCurrency;
-        logger.info("Exchange " + xName + " initialized with base url:" + xBaseUrl);
+    private static final Logger logger = Testslf4j.getLogger(ExchangeX.class);
+    private TreeMap<String, String> attributesMap = new TreeMap<>();
+
+    public ExchangeX( TreeMap<String, String> xMap){
+        this.attributesMap.putAll(xMap);
+        logger.info(this.attributesMap.get(ExchangeEnum.EXHANGENAME.getValue()));
     }
     
-    public String getApiKey(){ return this.apiKey;}
-    public String getSecret(){return this.apiSecret;    }
-    public String getBaseUrl(){  return this.baseUrl; }
-    public String getExchangeName(){ return this.exchangeName; }
-    public String getCoinPair(){ return this.coin + "_" + this.currency; }
-    public String getCoin(){ return this.coin;}
-    public String getCurrency(){ return this.currency;}
+    public String getApiKey(){ return this.attributesMap.get(ExchangeEnum.APIKEY.getValue());}
+    public String getSecret(){ return this.attributesMap.get(ExchangeEnum.APISECRET.getValue());    }
+    public String getBaseUrl(){  return this.attributesMap.get(ExchangeEnum.BASEURL.getValue()); }
+    public String getExchangeName(){ return this.attributesMap.get(ExchangeEnum.EXHANGENAME.getValue()); }
+    public String getCoinPair(){ return this.attributesMap.get(ExchangeEnum.SYMBOL.getValue()) + "_" + this.attributesMap.get(ExchangeEnum.CURRENCY.getValue()); }
+    public String getCoin(){ return this.attributesMap.get(ExchangeEnum.SYMBOL.getValue());}
+    public String getCurrency(){ return this.attributesMap.get(ExchangeEnum.CURRENCY.getValue());}
 }
