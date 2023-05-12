f8f11b8b89f2ea6cec7c6ee265a9af42af63bffd
Prof-Cat <mimaopro@outlook.jp>
2023-05-12 22:22:51 +0900

    shift to TreeMap

--- src/main/java/com/metatech/crypto/exchange/Marketdata.java
+++ src/main/java/com/metatech/crypto/exchange/Marketdata.java
@@ -2,22 +2,7 @@ package com.metatech.crypto.exchange;
 
 import com.metatech.JavaCat.Testslf4j;
 import org.slf4j.Logger;
-import java.util.Properties;
-
-import org.apache.commons.codec.binary.Hex;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Map;
+import java.util.TreeMap;
 
 public class Marketdata {
     private String apiKey;
@@ -25,9 +10,9 @@ public class Marketdata {
     public static ExchangeX targetExchange;
     private static final Logger logger = Testslf4j.getLogger(Marketdata.class);
 
-    public Marketdata(String xExchange, Map<String, ExchangeX> theMap) {
+    public Marketdata(String xExchange, TreeMap<String, String> theMap) {
         try {
-            targetExchange = theMap.get(xExchange);
+            targetExchange =  new ExchangeX(theMap);
             apiKey = targetExchange.getApiKey();
             apiSecret = targetExchange.getSecret();
             logger.info("Marketdata initialized with " + xExchange);
