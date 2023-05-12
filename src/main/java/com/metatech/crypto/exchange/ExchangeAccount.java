42459f1fc16994effb40210c701635b9ae44c95d
Prof-Cat <mimaopro@outlook.jp>
2023-05-12 22:19:28 +0900

    TreeMap shift

--- src/main/java/com/metatech/crypto/exchange/ExchangeAccount.java
+++ src/main/java/com/metatech/crypto/exchange/ExchangeAccount.java
@@ -2,22 +2,8 @@ package com.metatech.crypto.exchange;
 
 import com.metatech.JavaCat.Testslf4j;
 import org.slf4j.Logger;
-import java.util.Properties;
 
-import org.apache.commons.codec.binary.Hex;
-
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Map;
+import java.util.TreeMap;
 
 public class ExchangeAccount {
     private String apiKey;
@@ -25,9 +11,9 @@ public class ExchangeAccount {
     public static ExchangeX targetExchange;
     private static final Logger logger = Testslf4j.getLogger(ExchangeAccount.class);
 
-    public ExchangeAccount(String xExchange, Map<String, ExchangeX> theMap) {
+    public ExchangeAccount(String xExchange, TreeMap<String, String> theMap) {
         try {
-            targetExchange = theMap.get(xExchange);
+            targetExchange = new ExchangeX(theMap);
             apiKey = targetExchange.getApiKey();
             apiSecret = targetExchange.getSecret();
             logger.info("ExchangeAccount initialized with " + xExchange);
