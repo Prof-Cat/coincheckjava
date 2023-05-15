c3d1445b3c36e60ff4cb8193c8c948df4f88dc64
Prof-Cat <mimaopro@outlook.jp>
2023-05-15 23:21:41 +0900

    simplify and remove hardcoding

--- src/main/java/com/metatech/crypto/exchange/LineHandler.java
+++ src/main/java/com/metatech/crypto/exchange/LineHandler.java
@@ -4,7 +4,6 @@ import com.metatech.JavaCat.Testslf4j;
 import com.metatech.crypto.exchange.TagMap.ExchangeEnum;
 
 import org.slf4j.Logger;
-import java.util.Map;
 import java.util.TreeMap;
 
 public class LineHandler {
@@ -27,77 +26,53 @@ public class LineHandler {
 
         // GET /api/exchange/orders/opens
         public String getOpenOrders() {
-            String url = targetExchange.getBaseUrl() + "exchange/orders/opens";
+            String url = targetExchange.opensEndPoint();
             String jsonString = Util.requestByUrlWithHeader(url, apiKey, apiSecret);
             return jsonString;
         }
     
         // GET /api/exchange/orders/transactions_pagination
         public String getTransactionPages() {
-            String url = targetExchange.getBaseUrl() + "exchange/orders/transactions_pagination";
+            String url = targetExchange.pagedTxsEndPoint();
             String jsonString = Util.requestByUrlWithHeader(url, apiKey, apiSecret); // requestByUrlWithHeader(url);
             return jsonString;
         }
     
-        // Send new order via exchenga/orders
-        // HTTP REQUEST :: POST /api/exchange/orders
-        // PARAMETERS
-        // *pair取引ペア。現在は btc_jpy, etc_jpy, lsk_jpy, mona_jpy, omg_jpy, plt_jpy, fnct_jpyが利用可能です。
-        // *order_type注文方法
-        // rate注文のレート。（例）28000
-        // amount注文での量。（例）0.1
-        // market_buy_amount成行買で利用する日本円の金額。（例）10000
-        // stop_loss_rate逆指値レート ( 逆指値とは？ )
-        // time_in_force注文有効期間(optional)。
-        // "good_til_cancelled"(キャンセルされるまで有効。デフォルト) 
-        // あるいは "post_only"( postonlyとは？ )が指定できます。
-        public String newOrder( double xPrice, double xQty, String xCoinPair, CoinCheckOrderType xOrderType ){
+        public String newOrder( PortfolioOrder myNewCashOrder ){
             String jsonString;
             // order price check, and qty check, etc should pass before reaching here
-            String url = targetExchange.getBaseUrl() + "exchange/orders";
-            // String bodyNewOrder = "{\"rate\":" + xPrice + ",\"amount\":" + xQty + ",\"order_type\":\"" + xOrderType.getValue() + "\",\"pair\":\"" + xCoinPair + "\"}";
-            // String bodyNewOrder = "{rate=" + xPrice + "&amount=" + xQty + "&order_type=" + xOrderType.getValue() + "&pair=" + xCoinPair + "}";
-            String bodyNewOrder = "rate=" + xPrice + "&amount=" + xQty + "&order_type=" + xOrderType.getValue() + "&pair=" + xCoinPair;
-            logger.info(Util.headerString(targetExchange.getExchangeName(), xCoinPair, "newOrder") + bodyNewOrder);
-            jsonString = Util.postByUrlWithHeader(url, apiKey, apiSecret, bodyNewOrder);
-            return jsonString;
-        }
-
-        public String newOrder( CryptoCashOrder myNewCashOrder ){
-            String jsonString;
-            // order price check, and qty check, etc should pass before reaching here
-            String url = targetExchange.getBaseUrl() + "exchange/orders";
+            String url = targetExchange.orderEndPoint();
 
             String bodyNewOrder = myNewCashOrder.buildPostBody();
-            logger.info(Util.headerString(targetExchange.getExchangeName(), targetExchange.getCoinPair(), "newOrder") + bodyNewOrder);
+            logger.info(Util.headerString(targetExchange.getExchangeName(), targetExchange.getCoinPair(), ExchangeEnum.NEWORDER.getValue()) + bodyNewOrder);
             jsonString = Util.postByUrlWithHeader(url, apiKey, apiSecret, bodyNewOrder);
             return jsonString;
         }
 
         // Cancel <id> : DELETE /api/exchange/orders/<id>
         public String cancelOrderID( String xId) {
-            String url = targetExchange.getBaseUrl() + "exchange/orders/" + xId;
+            String url = targetExchange.orderEndPoint() + xId;
             String jsonString = Util.deleteByUrlWithHeader(url, apiKey, apiSecret); // requestByUrlWithHeader(url);
             return jsonString;
         }
 
         // Cancel <id> : DELETE /api/exchange/orders/<id>
         public String cancelOrderID( Long xId) {
-            String url = targetExchange.getBaseUrl() + "exchange/orders/" + xId;
+            String url = targetExchange.orderEndPoint() + Long.toString(xId);
             String jsonString = Util.deleteByUrlWithHeader(url, apiKey, apiSecret); // requestByUrlWithHeader(url);
             return jsonString;
         }
         
         // Request <id> cancel status : GET /api/exchange/orders/cancel_status?id=[id]
         public String getCancelStatus( String xId) {
-            String url = targetExchange.getBaseUrl() + "exchange/orders/cancel_status?" + xId;
+            String url = targetExchange.cancelStatusEndPoint() + xId;
             String jsonString = Util.requestByUrlWithHeader(url, apiKey, apiSecret); // requestByUrlWithHeader(url);
             return jsonString;
         }
         
         // GET /api/exchange/orders/transactions
         public String getTransactions() {
-            String url = targetExchange.getBaseUrl() + "exchange/orders/transactions";
+            String url = targetExchange.transactionEndPoint();
             String jsonString = Util.requestByUrlWithHeader(url, apiKey, apiSecret); // requestByUrlWithHeader(url);
             return jsonString;
         }
