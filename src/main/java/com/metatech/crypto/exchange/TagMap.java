f6247403abae87df0cf1c7b9a51c8db4858f5946
Prof-Cat <mimaopro@outlook.jp>
2023-05-15 23:14:58 +0900

    enrich dictionary, following FIX protocol

--- src/main/java/com/metatech/crypto/exchange/TagMap.java
+++ src/main/java/com/metatech/crypto/exchange/TagMap.java
@@ -30,13 +30,25 @@ public class TagMap {
         SYMBOL("symbol"),
         BASEURL("BaseUrl"),
         EXHANGENAME("execName"),
-        REALTIME("realtime");
+        REALTIME("realtime"),
+        ORDERENDPOINT("endpoint_orders"),
+        OPENSENDPOINT("endpoint_opens"),
+        NEWORDER("newOrder"),
+        CANCELSTATUS("endpoint_cancelstatus"),
+        TXSENDPOINT("endpoint_txs"),
+        PAGEDTXS("endpoint_pagedtxs"),
+        ACCTBALLANCE("endpoint_acctbalance"),
+        ACCTINFO("endpoint_acctinfo"),
+        PUBTICKER("endpoint_pticker"),
+        PUBTRADES("endpoint_trades"),
+        ORDERBOOKS("endpoint_orderbook");
 
         private final String value;
         private ExchangeEnum(String value) { this.value = value;        }
         public String getValue() { return this.value;}
     }
 
+    // TOBE replaced
     public enum OrderStatusEnum {
         LOCAL("local"),
         PENDING("pending"),
@@ -53,6 +65,33 @@ public class TagMap {
         public String getValue() { return this.value;}
     }
 
+    public enum OrdStatusEnum {
+        LOCAL('L'),
+        PENDING('A'),
+        NEW('0'),
+        PARTIALFILL('1'),
+        FULLFILL('2'),
+        DFD('3'),
+        CANCELED('4'),
+        PCANCEL('5'),
+        EXPIRED('C'),
+        REJECTED('8');
+        // to keep siimple, not going to handle amendments, only new order, cancel
+    
+        private final char value;
+        private OrdStatusEnum( char value) { this.value = value;        }
+        public char getValue() { return this.value;}
+
+        public static OrdStatusEnum fromValue(char value) {
+            for (OrdStatusEnum myEnum : OrdStatusEnum.values()) {
+                if (myEnum.value == value) {
+                    return myEnum;
+                }
+            }
+            throw new IllegalArgumentException("Invalid value: " + value);
+        }
+    }
+
     public enum TimeInForceEnum{
         DAY(0),
         GTC(1),
@@ -66,5 +105,51 @@ public class TagMap {
         private final Integer value;
         private TimeInForceEnum(Integer xValue) { this.value = xValue;        }
         public Integer getValue() { return this.value;}
+    }
+
+    public enum ExDestination{
+        COINCHECK("CoinCheck"),
+        ZAIF("Zaif"),
+        GMO("GMO"),
+        BINANCE("Binance"),
+        OKEX("OKEX"),
+        MTX("Mtx");
+
+        private final String value;
+        private ExDestination(String xValue) { this.value = xValue;        }
+        public String getValue() { return this.value;}
+
+        public static ExDestination fromValue(String value) {
+            for (ExDestination myEnum : ExDestination.values()) {
+                if (myEnum.value.equals(value)) {
+                    return myEnum;
+                }
+            }
+            throw new IllegalArgumentException("Invalid value: " + value);
+        }
+    }
+
+    public enum BaseCurrency{
+        USD("USD"),
+        JPY("JPY"),
+        EURO("EURO"),
+        HKD("HKD"),
+        SGD("SGD"),
+        CNY("CNY"),
+        USDC("USDC"),
+        USDT("USDT");
+
+        private final String value;
+        private BaseCurrency(String xValue) { this.value = xValue;        }
+        public String getValue() { return this.value;}
+
+        public static BaseCurrency fromValue(String value) {
+            for (BaseCurrency myEnum : BaseCurrency.values()) {
+                if (myEnum.value.equals(value)) {
+                    return myEnum;
+                }
+            }
+            throw new IllegalArgumentException("Invalid value: " + value);
+        }
     }
 }
