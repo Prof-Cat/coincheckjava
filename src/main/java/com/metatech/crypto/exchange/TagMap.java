6cc60e3818f791f3537d1e25f2630e5209fcd64f
junjiequ123 <junjie.qu@qq.com>
2023-05-23 11:58:39 +0800

    temp solution, introducting FIX32.xml to adopt FIX later

--- src/main/java/com/metatech/crypto/exchange/TagMap.java
+++ src/main/java/com/metatech/crypto/exchange/TagMap.java
@@ -2,11 +2,15 @@ package com.metatech.crypto.exchange;
 
 public class TagMap {
 
+    public TagMap(){
+        // to avoid implicit constructor error
+    }
+    
     public enum CmcattributesEnum{
         APIKEYHEADER("apiKeyHeader"),
         APIKEY("apiKey"),
         CURRENCY("currency"),
-        SYMBOL("symbol"),
+        COIN("symbol"),
         BASEURL("BaseUrl"),
         IDMAP("IDMap"),
         METADATAV2("MetaDataV2"),
@@ -23,11 +27,11 @@ public class TagMap {
         public String getValue() { return this.value;}
     }
 
-    public enum ExchangeEnum{
+    public enum ExchangeAccessEnum{
         APISECRET("apiSecret"),
         APIKEY("apiKey"),
         CURRENCY("currency"),
-        SYMBOL("symbol"),
+        COIN("symbol"),
         BASEURL("BaseUrl"),
         EXHANGENAME("execName"),
         REALTIME("realtime"),
@@ -44,12 +48,12 @@ public class TagMap {
         ORDERBOOKS("endpoint_orderbook");
 
         private final String value;
-        private ExchangeEnum(String value) { this.value = value;        }
+        private ExchangeAccessEnum(String value) { this.value = value;        }
         public String getValue() { return this.value;}
     }
 
     // TOBE replaced
-    public enum OrderStatusEnum {
+    public enum OrderStatusNameEnum {
         LOCAL("local"),
         PENDING("pending"),
         NEW("new"),
@@ -61,7 +65,7 @@ public class TagMap {
         // to keep siimple, not going to handle amendments, only new order, cancel
     
         private final String value;
-        private OrderStatusEnum(String value) { this.value = value;        }
+        private OrderStatusNameEnum(String value) { this.value = value;        }
         public String getValue() { return this.value;}
     }
 
@@ -76,7 +80,7 @@ public class TagMap {
         PCANCEL('5'),
         EXPIRED('C'),
         REJECTED('8');
-        // to keep siimple, not going to handle amendments, only new order, cancel
+        // to keep simple, not going to handle amendments, only new order, cancel
     
         private final char value;
         private OrdStatusEnum( char value) { this.value = value;        }
@@ -90,6 +94,18 @@ public class TagMap {
             }
             throw new IllegalArgumentException("Invalid value: " + value);
         }
+        // public static String nameOfValue(char value){
+        //     switch (value){
+        //         case OrdStatusEnum.CANCELED.getValue() :
+        //             return OrderStatusNameEnum.CANCELED.getValue();
+        //             break;
+        //         case OrdStatusEnum.REJECTED.getValue():
+        //             return 
+        //             break;
+        //         default:
+        //             break;
+        //     }
+        // }
     }
 
     public enum TimeInForceEnum{
